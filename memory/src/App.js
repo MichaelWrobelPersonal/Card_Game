31e7ea7eb4cbc60298a134c64bb57560ec564142
Michael Wrobel <michaelrwrobel@sbcglobal.net>
2018-06-13 00:11:44 -0400

    Added Random Shuffle Code - Needs more debugging

--- memory/src/App.js
+++ memory/src/App.js
@@ -3,26 +3,45 @@ import Card from "./components/Card";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import cards from "./cards.json";
-import "./App.css";
+import score from "./components/Score";
+import "./App.css"; 
 
-class App extends Component {
+class App extends Component { 
   // Setting this.state.cards to the cards json array
   state = {
-    cards
+    cards,
+    score
   };
 
   removeCard = id => {
     // Filter this.state.cards for cards with an id not equal to the id being removed
     const cards = this.state.cards.filter(card => card.id !== id);
+    let score = this.state.score;
+
+    score += 1;
+
     // Set this.state.cards equal to the new cards array
-    this.setState({ cards });
+    this.shuffleCards()
+    this.setState({ cards, score });
   };
 
+  getImageUrl = id => {
+     const card = this.state.cards.filter(card => card.id === id);
+     return card.image;
+
+  };
+
+  setImageUrl = (id, image) => {
+    const cards = this.state.cards;
+    const card = cards.filter(cards => card.id === id);
+    card.setState({ image });
+  };
   // Map over this.state.cards and render a FriendCard component for each card object
   render() {
     return (
       <Wrapper>
         <Title>Memory Game</Title>
+        <Score>{this.state.score}</Score>
         {this.state.cards.map(card => (
           <Card
             removeCard={this.removeCard}
@@ -32,11 +51,80 @@ class App extends Component {
             image={card.image}
             suit={card.suit}
             face={card.face}
+            location={this.getRandomCard()}
           />
         ))}
       </Wrapper>
     );
   }
-}
 
+  getRandomCard = function() {
+    return cards[Math.floor(Math.random() * 2)];
+  };
+
+  shuffle = (array) => {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+  
+  // Example of how it will be used,
+  // 1) On load if the game
+  //    a) Allow cards to be rendered unsorted, reset the score
+  //    b) Build array of images from the unsorted cards
+  //    c) Call the random shuffle method above
+  //    d) Use resuting array to set images in the cards.
+  onLoad = (event) =>
+  {
+    this.shuffleCards();
+  }
+  
+  onRemove = (event) =>
+  {
+    this.shuffleCards();
+  }
+
+  shuffleCards = () =>
+  {
+    let images = [];
+    let key = 0;
+    for(key=0;key<cards.length; key++)
+    {
+      images[key] = this.getImageUrl(key);
+      console.log('Preimage: ' + images[key]);
+    }
+
+    console.log('Pre shuffle: ' + cards);
+    images = this.shuffle(images);
+    console.log('Post shuffle: ' + cards);
+
+    for(key=0;key<cards.length; key++)
+    {
+      if (images[key] != null)
+      {
+        console.log('Postimage: ' + images[key]);
+        this.setImageUrl(key, images[key]);
+      }
+    }
+  }
+
+  // 2) On deletion of a card,
+  //    a) Card is removed, and score is updated
+  //    b) Build array of images from remainig cards
+  //    c) Call the random shuffle method above
+  //    d) Use resuting array to set images in the cards.  
+}
 export default App;
