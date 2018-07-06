59c989756268d1e4f79201eacb59ab8ae804295a
Michael Wrobel <michaelrwrobel@sbcglobal.net>
2018-07-05 23:02:34 -0400

    Resolved the card shuffle issue

--- src/App.js
+++ src/App.js
@@ -10,54 +10,33 @@ import "./App.css";
 class App extends Component { 
   // Setting this.state.cards to the cards json array
   state = {
-    cards,
     score
   };
 
   removeCard = id => {
-    let shuffled = this.shuffleCards();
 
     // Filter this.state.cards for cards with an id not equal to the id being removed
-    let cards = this.state.cards.filter(card => card.id !== id);
+    cards.filter(card => card.id !== id);
     let score = this.state.score;
-    let shuffledCards = cards;
     score += 1;
-    for (let i=0;i<cards.length;i++)
-    {
-      console.log("Cards_--", cards[shuffled[i]]);
-      if( cards[shuffled[i] != null])
-      {
-        let card = this.state.cards.filter(card => card.id === shuffled[i]);
-        card.id = cards[shuffled[i].id];
-        card.location = cards[shuffled[i].location];
-        card.image = cards[shuffled[i].image];
-        card.face = cards[shuffled[i].face];
-        card.suit = cards[shuffled[i].suit];
-        console.log('Cards_---', card); 
-        shuffledCards[i] = card;      
-      }
-    }
 
     // Set this.state.cards equal to the new cards array
-    console.log('Cards-___', shuffledCards)
-    this.setState({ shuffledCards, score });
-    console.log('Cards____', shuffledCards)
+    this.setState({ score });
   };
 
   getImageUrl = id => {
-     let card = this.state.cards.filter(card => card.id === id);
+     let card = cards.filter(card => card.id === id);
      console.log('card ', card)
      return  card[0] ? card[0].image : null;
   };
 
   getImageLocation = id => {
-    let card = this.state.cards.filter(card => card.id === id);
+    let card = cards.filter(card => card.id === id);
     console.log('card ', card)
     return  card[0] ? card[0].location : null;
   };
 
   setImageUrl = (id, image) => {
-    let cards = this.state.cards;
     let card = cards.filter(card => card.id === id);
     console.log('image_', image );
     console.log('card_', card);
@@ -66,28 +45,29 @@ class App extends Component {
     console.log('card_- ', card);
   };
 
-  setImageLocation = (id, location) => {
-    let cards = this.state.cards;
-    let card = cards.filter(card => card.id === id);
-    console.log('location_', location );
-    console.log('card_', card);
-    card.location = location;
-    card.id = location;
-    this.setState({ card }); 
-    console.log('card_- ', card);
-  };
+  shuffleArray(array) {
+    let i = array.length - 1;
+    for (; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  }
 
   // Map over this.state.cards and render a FriendCard component for each card object
   render() {
+    const shuffledCards = this.shuffleArray(cards); 
     return (
       <Wrapper>
         <Title>Memory Game</Title>
         <Score>{this.state.score}</Score>
-        {this.state.cards.map(card => (
+        {shuffledCards.map((card,idx) => (
           <Card
             removeCard={this.removeCard}
             id={card.id}
-            key={card.id}
+            key={idx}
             name={card.name}
             image={card.image}
             suit={card.suit}
@@ -122,6 +102,7 @@ class App extends Component {
     return array;
   }
   
+
   // Example of how it will be used,
   // 1) On load if the game
   //    a) Allow cards to be rendered unsorted, reset the score
@@ -140,34 +121,17 @@ class App extends Component {
 
   shuffleCards = () =>
   {
-    let unshuffled = [];
-    let shuffled = [];
-    let key = 0;
-    for(key=0;key<cards.length; key++)
-    {
-      unshuffled[key] = this.getImageLocation(key);
-      console.log('PreLocation: ' + unshuffled[key]);
-    }
+    console.log('Pre shuffle: ' + cards);
+    let shuffledCards = this.shuffleArray(cards);
+    console.log('Post shuffle: ' + shuffledCards);
 
-    console.log('Pre shuffle: ' + unshuffled);
-    shuffled = this.shuffle(unshuffled);
-    console.log('Post shuffle: ' + shuffled);
-
-    for(key=0;key<cards.length; key++)
-    {
-      if (shuffled[key] != null)
-      {
-        console.log('PostLocation: ' + shuffled[key]);
-        this.setImageLocation(key, shuffled[key]);
-      }
-    }
-    return shuffled;
+    return shuffledCards;
   }
 
   // 2) On deletion of a card,
   //    a) Card is removed, and score is updated
   //    b) Build array of images from remainig cards
-  //    c) Call the random shuffle method above
+  //    c) Ca ll the random shuffle method above
   //    d) Use resuting array to set images in the cards.  
 }
 export default App;
